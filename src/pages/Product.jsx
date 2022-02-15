ada0c0ca417c58ef6f654dc584a2f4436e3a9016
sunilkarki <karkisunil1200@gmail.com>
2022-02-15 16:04:00 -0600

    Added product page

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,14 +1,78 @@
+import { Add, RemoveCircleOutline } from "@mui/icons-material";
+import AddIcon from "@mui/icons-material/Add";
+import RemoveIcon from "@mui/icons-material/Remove";
 import Announcement from "../components/Accouncement";
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
 import NewsLetter from "../components/NewsLetter";
-import { Container } from "./styles/productPage";
+import {
+  Container,
+  Wrapper,
+  ImgContainer,
+  Image,
+  InfoContainer,
+  Title,
+  Desc,
+  Price,
+  FilterContainer,
+  Filter,
+  FilterTitle,
+  FilterColor,
+  FilterSize,
+  FilterSizeOption,
+  AddContainer,
+  AmountContainer,
+  Amount,
+  Button,
+} from "./styles/productPage";
 
 const Product = () => {
   return (
     <Container>
       <Navbar />
       <Announcement />
+      <Wrapper>
+        <ImgContainer>
+          <Image src="https://images6.alphacoders.com/319/319175.jpg" />
+        </ImgContainer>
+        <InfoContainer>
+          <Title>Rock the World</Title>
+          <Desc>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dicta
+            tempora voluptatum similique ipsa iste quaerat eos explicabo id
+            illum cupiditate vero, itaque temporibus possimus recusandae vel
+            aliquid quo ab at accusantium. Et eos voluptatum id reiciendis
+            cumque obcaecati modi beatae.
+          </Desc>
+          <Price>$ 10</Price>
+          <FilterContainer>
+            <Filter>
+              <FilterTitle>Color</FilterTitle>
+              <FilterColor color="black" />
+              <FilterColor color="tomato" />
+              <FilterColor color="gray" />
+            </Filter>
+            <Filter>
+              <FilterTitle>Size</FilterTitle>
+              <FilterSize>
+                <FilterSizeOption>XS</FilterSizeOption>
+                <FilterSizeOption>S</FilterSizeOption>
+                <FilterSizeOption>M</FilterSizeOption>
+                <FilterSizeOption>L</FilterSizeOption>
+                <FilterSizeOption>XL</FilterSizeOption>
+              </FilterSize>
+            </Filter>
+          </FilterContainer>
+          <AddContainer>
+            <AmountContainer>
+              <AddIcon />
+              <Amount>2</Amount>
+              <RemoveIcon />
+            </AmountContainer>
+            <Button>ADD TO CART</Button>
+          </AddContainer>
+        </InfoContainer>
+      </Wrapper>
       <NewsLetter />
       <Footer />
     </Container>
--- src/pages/styles/productPage.js
+++ src/pages/styles/productPage.js
@@ -1,3 +1,112 @@
 import styled from "styled-components";
 
 export const Container = styled.div``;
+
+export const Wrapper = styled.div`
+  padding: 50px;
+  display: flex;
+`;
+
+export const ImgContainer = styled.div`
+  flex: 1;
+`;
+
+export const Image = styled.img`
+  width: 100%;
+  height: 80vh;
+  object-fit: cover;
+`;
+
+export const InfoContainer = styled.div`
+  flex: 1;
+  padding: 0px 50px;
+`;
+
+export const Title = styled.h1`
+  font-weight: 200;
+  font-size: 40px;
+`;
+
+export const Desc = styled.p`
+  margin: 20px 0px;
+  font-size: 22px;
+`;
+
+export const Price = styled.span`
+  font-weight: 100;
+  font-size: 40px;
+`;
+
+export const FilterContainer = styled.div`
+  width: 50%;
+  margin: 30px 0;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+export const Filter = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+export const FilterTitle = styled.span`
+  font-size: 20px;
+  font-weight: 200;
+`;
+
+export const FilterColor = styled.div`
+  height: 20px;
+  width: 20px;
+  border-radius: 50%;
+  background-color: ${(props) => props.color};
+  margin: 0px 5px;
+  cursor: pointer;
+`;
+export const FilterSize = styled.select`
+  margin-left: 10px;
+  padding: 10px;
+  cursor: pointer;
+  outline: none;
+`;
+
+export const FilterSizeOption = styled.option``;
+
+export const AddContainer = styled.div`
+  width: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+export const AmountContainer = styled.div`
+  display: flex;
+  align-items: center;
+  font-weight: 700;
+`;
+
+export const Amount = styled.span`
+  width: 30px;
+  height: 30px;
+  border-radius: 10px;
+  border: 1px solid teal;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 0 5px;
+  cursor: pointer;
+`;
+export const Button = styled.button`
+  padding: 10px;
+  cursor: pointer;
+  opacity: 0.6;
+  border: 2px solid teal;
+  background-color: white;
+  font-weight: 500;
+  transition: all 0.5s ease;
+
+  &:hover {
+    background-color: teal;
+    opacity: 0.8;
+    color: white;
+  }
+`;
