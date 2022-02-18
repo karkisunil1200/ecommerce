5abf4c86c49642a7165cc99e10896c596fef1260
sunilkarki <karkisunil1200@gmail.com>
2022-02-18 09:11:45 -0600

    Added styles for cart page

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,3 +1,4 @@
+import { Add, Remove } from "@mui/icons-material";
 import React from "react";
 import Announcement from "../components/Accouncement";
 import Footer from "../components/Footer";
@@ -11,6 +12,20 @@ import {
   TopButton,
   TopTexts,
   TopText,
+  Info,
+  Summary,
+  ProductDetail,
+  Image,
+  Details,
+  Product,
+  ProductName,
+  ProductId,
+  ProductColor,
+  ProductSize,
+  PriceDetail,
+  ProductAmountContainer,
+  ProductAmount,
+  ProductPrice,
 } from "./styles/cartPage";
 
 const Cart = () => {
@@ -28,7 +43,36 @@ const Cart = () => {
           </TopTexts>
           <TopButton type="filled">Checkout Now</TopButton>
         </Top>
-        <Bottom></Bottom>
+        <Bottom>
+          <Info>
+            <Product>
+              <ProductDetail>
+                <Image src="https://images5.alphacoders.com/641/641999.jpg" />
+                <Details>
+                  <ProductName>
+                    <b>Product: </b> Awesome Rain
+                  </ProductName>
+                  <ProductId>
+                    <b>ID: </b> 92384293485
+                  </ProductId>
+                  <ProductColor color="black" />
+                  <ProductSize>
+                    <b>Size</b> 23.5
+                  </ProductSize>
+                </Details>
+              </ProductDetail>
+              <PriceDetail>
+                <ProductAmountContainer>
+                  <Add />
+                  <ProductAmount>2</ProductAmount>
+                  <Remove />
+                </ProductAmountContainer>
+                <ProductPrice>$30.99</ProductPrice>
+              </PriceDetail>
+            </Product>
+          </Info>
+          <Summary>Summary</Summary>
+        </Bottom>
       </Wrapper>
       <Footer />
     </Container>
--- src/pages/styles/cartPage.js
+++ src/pages/styles/cartPage.js
@@ -13,16 +13,15 @@ export const Top = styled.div`
   align-items: center;
   justify-content: space-between;
   padding: 20px;
-  border: 2px solid red;
 `;
 
 export const TopButton = styled.button`
   padding: 10px;
   font-weight: 600;
   cursor: pointer;
-  border: ${(props) => props.type == "filled" && "none"};
+  border: ${(props) => props.type === "filled" && "none"};
   background-color: ${(props) =>
-    props.type == "filled" ? "black" : "transparent"};
+    props.type === "filled" ? "black" : "transparent"};
   color: ${(props) => props.type === "filled" && "white"};
 `;
 
@@ -34,4 +33,70 @@ export const TopText = styled.span`
   margin: 0px 10px;
 `;
 
-export const Bottom = styled.div``;
+export const Bottom = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+
+export const Info = styled.div`
+  flex: 3;
+`;
+
+export const Product = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+export const ProductDetail = styled.div`
+  display: flex;
+  flex: 2;
+`;
+
+export const Image = styled.img`
+  width: 350px;
+  object-fit: cover;
+`;
+
+export const Details = styled.div`
+  padding: 20px;
+  display: flex;
+  justify-content: space-around;
+  flex-direction: column;
+`;
+
+export const ProductName = styled.span``;
+
+export const ProductId = styled.span``;
+
+export const ProductColor = styled.div`
+  width: 20px;
+  height: 20px;
+  border-radius: 50%;
+  background-color: ${(props) => props.color};
+`;
+export const ProductSize = styled.div``;
+
+export const PriceDetail = styled.div`
+  flex: 1;
+  flex-direction: column;
+  justify-content: center;
+  display: flex;
+`;
+
+export const ProductAmountContainer = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+export const ProductPrice = styled.div`
+  font-size: 30px;
+  font-weight: 200;
+`;
+
+export const ProductAmount = styled.div`
+  font-size: 24px;
+  margin: 5px;
+`;
+
+export const Summary = styled.div`
+  flex: 1;
+`;
